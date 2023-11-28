df0069fb0b9261abd5cebaf0304fe3fa83fc3260
Parvej-Hoque-Palash <cloudnumber981@gmail.com>
2023-11-28 13:36:00 +0600

    added jsdoc

---
+++ src/index.js
@@ -0,0 +1,23 @@
+/**
+ * only for test
+ * @returns integer number
+ */
+
+function test(){
+    return 10;
+}
+/**
+ * an integer number
+ * @type {number}
+ */
+var d1 = 6;
+/**
+ * 
+ * @param {number} d1 
+ * @returns return an integer number
+ */
+function test2(d1){
+    return d1*10;
+}
+
+console.log(test2);
